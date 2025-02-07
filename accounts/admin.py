0b85431548da09b4fa0220e1deca0dcd178bf8e3
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-02-07 14:47:21 +0600

    Update CustomUserAdmin to include is_verified field and adjust fieldsets for improved user management

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,36 +1,24 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
-    # List the fields to display in the admin list view
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active','role')
-    
-    # Fields to search in the admin list view
+    model = CustomUser
+    list_display = ( 'email','id', 'username', 'is_verified', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active', 'is_verified')
     search_fields = ('email', 'username')
-    
-    # Fields to filter the list by
-    list_filter = ('is_staff', 'is_active', 'date_joined')
-    
-    # Customize the form used to edit users
+    ordering = ('email',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('role','is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_verified', 'role', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
     )
-    
-    # Specify which fields are required for user creation
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2'),
-        }),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active', 'role')}
+        ),
     )
-    
-    # Define the ordering of users in the admin list
-    ordering = ('email',)
 
-# Register the custom user model with the customized admin
 admin.site.register(CustomUser, CustomUserAdmin)
